aaea2eca83ac0282e84743bc5a6df6e1cd94ac8b
vinidevclubpos <devclubpos@gmail.com>
2023-10-02 13:30:05 -0300

    fixed path image

--- src/Components/Jobs/dataJob.js
+++ src/Components/Jobs/dataJob.js
@@ -1,36 +1,36 @@
 const dataJob = [
     {
-        img: '/src/assets/iconContruction.svg',
+        img: './src/assets/iconContruction.svg',
         text: 'Construção',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: '/src/assets/iconRenovation.svg',
+        img: './src/assets/iconRenovation.svg',
         text: 'Renovação',
         bg: 'primary',
         textColor: 'white',
     }, 
     {
-        img: '/src/assets/iconConsolitation.svg',
+        img: './src/assets/iconConsolitation.svg',
         text: 'Consolidação',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: '/src/assets/iconService.svg',
+        img: './src/assets/iconService.svg',
         text: 'Reparo de serviços',
         bg: 'primary',
         textColor: 'white',
     }, 
     {
-        img: '/src/assets/iconArcthect.svg',
+        img: './src/assets/iconArcthect.svg',
         text: 'Arquitetura',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: '/src/assets/iconElectric.svg',
+        img: './src/assets/iconElectric.svg',
         text: 'Estrutura Elétrica',
         bg: 'primary',
         textColor: 'white',
