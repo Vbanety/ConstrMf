c9fe5c5dd9a8f3a13c69cf18213baf78e65f49b9
vinidevclubpos <devclubpos@gmail.com>
2023-10-02 14:38:59 -0300

    fixed path to public for icons services

--- src/Components/Jobs/dataJob.js
+++ src/Components/Jobs/dataJob.js
@@ -1,36 +1,36 @@
 const dataJob = [
     {
-        img: './src/assets/iconContruction.svg',
+        img: './iconContruction.svg',
         text: 'Construção',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: './src/assets/iconRenovation.svg',
+        img: './iconRenovation.svg',
         text: 'Renovação',
         bg: 'primary',
         textColor: 'white',
     }, 
     {
-        img: './src/assets/iconConsolitation.svg',
+        img: './iconConsolitation.svg',
         text: 'Consolidação',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: './src/assets/iconService.svg',
+        img: './iconService.svg',
         text: 'Reparo de serviços',
         bg: 'primary',
         textColor: 'white',
     }, 
     {
-        img: './src/assets/iconArcthect.svg',
+        img: './iconArcthect.svg',
         text: 'Arquitetura',
         bg: 'white',
         textColor: 'primary',
     }, 
     {
-        img: './src/assets/iconElectric.svg',
+        img: './iconElectric.svg',
         text: 'Estrutura Elétrica',
         bg: 'primary',
         textColor: 'white',
--- src/Components/Projects/dataProjects.js
+++ src/Components/Projects/dataProjects.js
@@ -5,27 +5,27 @@ const dataProjects = [
         address: 'Av: Campos do Alemães, 351 - Jd Souto - SJC',
     }, 
     {
-        img: './src/assets/project2.png',
+        img: './project2.png',
         title: 'Centro tecnológico',
         address: 'R: Natal, 41 - Centro - Jacareí',
     }, 
     {
-        img: './src/assets/project3.png',
+        img: './project3.png',
         title: 'Banco Central',
         address: 'R: Principal, 4241 - Limoeiro - São Paulo',
     }, 
     {
-        img: './src/assets/project4.png',
+        img: './project4.png',
         title: 'Centro Cultural',
         address: 'Rua: Martins de Sá, 320 - Praia Nobina - Caraguagatauba',
     }, 
     {
-        img: './src/assets/iconArcthect.svg',
+        img: './iconArcthect.svg',
         title: 'white',
         address: 'Arquitetura',
     }, 
     {
-        img: './src/assets/iconElectric.svg',
+        img: './iconElectric.svg',
         title: 'primary',
         address: 'Estrutura Elétrica',
     }   
